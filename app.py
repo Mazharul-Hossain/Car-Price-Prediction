2c82e117418b678cbe3cbfbbcb3046a427bd7a2b
Mazhar <1526155-Mazharul-Hossain@users.noreply.gitlab.com>
2020-11-20 04:14:07 -0600

    Update app.py to serve static files

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask,request,render_template,send_file,make_response,url_for
+import pickle
 from io import BytesIO
-import base64
+
 import numpy as np
 import pandas as pd
-import pickle
-#from flask import Flask, 
+from flask import Flask, request, render_template, send_file
+
+# from flask import Flask,
 from plotting import price_graph
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static')
 model = pickle.load(open('linear.pkl', 'rb'))
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/hello')
 def hello():
     return '<h3>Hello World!....it works....testing</h3>'
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/bootstrap-elements')
 def bootstrap():
@@ -28,31 +34,30 @@ def bootstrap():
 
 @app.route('/predicting-car-price')
 def car_price():
-    read_data=pd.read_csv('data/true_car_listings.csv')
-    city=read_data.City.unique()
-    state=read_data.State.unique()
-    make=read_data.Make.unique()
-    model=read_data.Model.unique()
-    return render_template('car-price.html',city=city ,state=state ,make=make , model=model)
+    read_data = pd.read_csv('data/true_car_listings.csv')
+    city = read_data.City.unique()
+    state = read_data.State.unique()
+    make = read_data.Make.unique()
+    model = read_data.Model.unique()
+    return render_template('car-price.html', city=city, state=state, make=make, model=model)
+
 
 @app.route('/plots/car_price_data/correlation_matrix')
 def images():
-    return render_template("home.html", title= 'testing')
+    return render_template("home.html", title='testing')
 
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    #print(final_features)
+    # print(final_features)
     prediction = model.predict(final_features)
-    #return render_template('car-price.html', prediction_text=final_features)
+    # return render_template('car-price.html', prediction_text=final_features)
 
     output = round(prediction[0], 2)
 
     return render_template('car-price.html', prediction_text='Car price should be $ {}'.format(output))
-
 
 
 @app.route('/correlation_matrix')
@@ -62,14 +67,13 @@ def correlation_matrix():
     bytes_obj.savefig(img)
     img.seek(0)
     return send_file(img, mimetype='image/png')
-    #plot= base64.b64encode(img.getvalue()).decode('utf8')
-    
-    
-    #render_template('home.html', title= "testing", plot=plot)
-    #return send_file(img, mimetype='image/png')
-    #return send_file(bytes_obj,
-                    # attachment_filename='plot.png',
-                    # mimetype='image/png')
+    # plot= base64.b64encode(img.getvalue()).decode('utf8')
+
+    # render_template('home.html', title= "testing", plot=plot)
+    # return send_file(img, mimetype='image/png')
+    # return send_file(bytes_obj,
+    # attachment_filename='plot.png',
+    # mimetype='image/png')
 
 
 if __name__ == '__main__':
