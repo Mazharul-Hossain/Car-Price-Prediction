12ff6b7192ad927f2aad37fe05e11ee524a5fd83
Mazhar <1526155-Mazharul-Hossain@users.noreply.gitlab.com>
2020-11-19 22:04:52 -0600

    Update python files to run training

---
+++ utils_model.py
@@ -0,0 +1,324 @@
+import json
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor, \
+    ExtraTreesRegressor, RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier
+# models
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error, balanced_accuracy_score, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.svm import SVR, LinearSVR, SVC
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+
+data_info = {}
+regressor_names = ["Linear Regression", "Linear SVR", "SVR", "SGD Regressor", "Decision Tree Regressor",
+                   "Random Forest Regressor", "Extra Trees Regressor", "Bagging Regressor",
+                   "Gradient Boosting Regressor", "Ada Boost Regressor"]
+regressors = [
+    LinearRegression(n_jobs=-1),
+    LinearSVR(),
+    SVR(max_iter=1000),
+    SGDRegressor(early_stopping=True),
+    DecisionTreeRegressor(max_depth=10),
+    RandomForestRegressor(max_depth=10, n_estimators=10, max_features=1, n_jobs=-1),
+    ExtraTreesRegressor(max_depth=10, n_estimators=10, max_features=1, n_jobs=-1),
+    BaggingRegressor(max_features=1, n_jobs=-1),
+    GradientBoostingRegressor(max_features=1),
+    AdaBoostRegressor()]
+
+classifier_names = ["Nearest Neighbors Classifier",
+                    "Linear SVM Classifier", "RBF SVM Classifier",
+                    "Gaussian Process Classifier",
+                    "Decision Tree Classifier", "Random Forest Classifier", "Extra Trees Classifier",
+                    "Neural Net Classifier", "AdaBoost Classifier",
+                    "Naive Bayes Classifier", "Quadratic Discriminant Analysis (QDA) Classifier"]
+classifiers = [
+    KNeighborsClassifier(n_jobs=-1),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0), n_jobs=-1),
+    DecisionTreeClassifier(max_depth=10),
+    RandomForestClassifier(max_depth=10, n_estimators=10, max_features=1, n_jobs=-1),
+    ExtraTreesClassifier(max_depth=10, n_estimators=10, max_features=1, n_jobs=-1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()]
+
+
+# #####################################################
+def first_run():
+    global data_info
+    json_file = "notebooks/tmp/data_info.json"
+
+    if data_info is None:
+        data_info = {}
+    if len(data_info) == 0:
+        temp_info = json_info(json_file, dump=False)
+
+        if temp_info is not None:
+            data_info = temp_info
+
+    if 'csv_file' not in data_info:
+        data_info['csv_file'] = "notebooks/datasets/true_car_listings.csv"
+
+    if 'json_file' not in data_info:
+        data_info['json_file'] = json_file
+
+    # regression file
+    if 'regression' not in data_info:
+        data_info['regression'] = "notebooks/tmp/regression"
+
+    if not os.path.exists(data_info['regression']):
+        os.makedirs(data_info['regression'])
+    if not os.path.isdir(data_info['regression']):
+        raise Exception("Sorry, {} is not a directory!".format(data_info['regression']))
+
+    for name in regressor_names:
+        if name not in data_info:
+            data_info[name] = name + ".pkl"
+
+    # classification file
+    if 'classification' not in data_info:
+        data_info['classification'] = "notebooks/tmp/classification"
+
+    if not os.path.exists(data_info['classification']):
+        os.makedirs(data_info['classification'])
+    if not os.path.isdir(data_info['classification']):
+        raise Exception("Sorry, {} is not a directory!".format(data_info['classification']))
+
+    for name in classifier_names:
+        if name not in data_info:
+            data_info[name] = name + ".pkl"
+
+    if 'num_class' not in data_info:
+        data_info['num_class'] = 10
+
+    if 'label_encoder' not in data_info:
+        data_info['label_encoder'] = "notebooks/tmp/label_encoder.pkl"
+
+    if 'scaler' not in data_info:
+        data_info['scaler'] = "notebooks/tmp/scaler.pkl"
+
+    json_info(data_info['json_file'])
+
+
+def json_info(json_file, dump=True):
+    if dump:
+        with open(json_file, 'w') as output_file:
+            json.dump(data_info, output_file, ensure_ascii=False, indent=2)
+    else:
+        data = None
+        if os.path.exists(json_file):
+            with open(json_file) as f:
+                data = json.load(f)
+        return data
+
+
+def update_info(key, value):
+    global data_info
+
+    data_info[key] = value
+    json_info(data_info['json_file'])
+
+
+# #####################################################
+def acc_d(y_meas, pred_y):
+    # Relative error between predicted y_pred and measured y_meas values
+    return mean_absolute_error(y_meas, pred_y) * len(y_meas) / sum(abs(y_meas))
+
+
+def acc_rmse(y_meas, pred_y):
+    # RMSE between predicted y_pred and measured y_meas values
+    return (mean_squared_error(y_meas, pred_y)) ** 0.5
+
+
+def regression_accuracy(test_y, pred_y):
+    acc_train_r2_num = round(r2_score(test_y, pred_y) * 100, 2)
+    print('acc(r2_score) for training =', acc_train_r2_num)
+
+    acc_train_d_num = round(acc_d(test_y, pred_y) * 100, 2)
+    print('acc(relative error) for training =', acc_train_d_num)
+
+    acc_train_rmse_num = round(acc_rmse(test_y, pred_y) * 100, 2)
+    print('acc(rmse) for training =', acc_train_rmse_num)
+
+    return acc_train_r2_num, acc_train_rmse_num
+
+
+def regression_accuracy_model(rgs, train_x, test_x, train_y, test_y):
+    rgs.fit(train_x, train_y)
+
+    pred_train = rgs.predict(train_x)
+    pred_y = rgs.predict(test_x)
+
+    print("\n### training performance")
+    regression_accuracy(train_y, pred_train)
+
+    print("### Test performance")
+    return regression_accuracy(test_y, pred_y)
+
+
+def classification_accuracy(test_y, pred_y, balanced=True):
+    acc_train_r2_num = round(accuracy_score(test_y, pred_y) * 100, 2)
+    print('Accuracy score for training =', acc_train_r2_num)
+    if not balanced:
+        acc_train_r2_num = round(balanced_accuracy_score(test_y, pred_y) * 100, 2)
+        print('Balanced accuracy for training =', acc_train_r2_num)
+    return acc_train_r2_num
+
+
+def classification_accuracy_model(clf, train_x, test_x, train_y, test_y, balanced=True):
+    clf.fit(train_x, train_y)
+
+    pred_train = clf.predict(train_x)
+    pred_y = clf.predict(test_x)
+
+    print("\n### training performance")
+    classification_accuracy(train_y, pred_train, balanced)
+
+    print("### Test performance")
+    return classification_accuracy(test_y, pred_y, balanced)
+
+
+def handle_pickle(model, file_location, dump=True):
+    if dump:
+        pickle.dump(model, open(file_location, 'wb'))
+    else:
+        return pickle.load(open(file_location, 'rb'))
+
+
+def model_pickle(model_name, model, train_x, test_x, train_y, test_y, selector='regression', balanced=True):
+    model.fit(train_x, train_y)
+    pred_y = model.predict(test_x)
+
+    print("\n### Test performance before pickling")
+    if selector == 'regression':
+        regression_accuracy(test_y, pred_y)
+    else:
+        classification_accuracy(test_y, pred_y, balanced)
+    handle_pickle(model, data_info[model_name], dump=True)
+
+    loaded_model = handle_pickle(model, data_info[model_name], dump=False)
+    pred_y = loaded_model.predict(test_x)
+
+    print("### Test performance after pickling")
+    if selector == 'regression':
+        regression_accuracy(test_y, pred_y)
+    else:
+        classification_accuracy(test_y, pred_y, balanced)
+
+
+def save_dataset_file(train0, test0, train_target0, test_target0, selector='regression'):
+    # https://stackoverflow.com/a/3685339/2049763
+    np.savetxt(os.path.join(data_info[selector], 'train0.txt'), train0)
+    np.savetxt(os.path.join(data_info[selector], 'test0.txt'), test0)
+
+    np.savetxt(os.path.join(data_info[selector], 'train_target0.txt'), train_target0)
+    np.savetxt(os.path.join(data_info[selector], 'test_target0.txt'), test_target0)
+
+
+def load_dataset_file(selector='regression'):
+    train0, test0, train_target0, test_target0 = None, None, None, None
+
+    if os.path.exists(os.path.join(data_info[selector], 'train0.txt')):
+        train0 = np.loadtxt(os.path.join(data_info[selector], 'train0.txt'))
+
+    if os.path.exists(os.path.join(data_info[selector], 'train0.txt')):
+        test0 = np.loadtxt(os.path.join(data_info[selector], 'test0.txt'))
+
+    if os.path.exists(os.path.join(data_info[selector], 'train0.txt')):
+        train_target0 = np.loadtxt(os.path.join(data_info[selector], 'train_target0.txt'))
+
+    if os.path.exists(os.path.join(data_info[selector], 'train0.txt')):
+        test_target0 = np.loadtxt(os.path.join(data_info[selector], 'test_target0.txt'))
+
+    return train0, test0, train_target0, test_target0
+
+
+def load_dataset_frame():
+    vehicles = pd.read_csv(data_info['csv_file'])
+
+    vehicles = vehicles.loc[(vehicles.Year >= 1970) & (vehicles.Price >= 1000) & (
+            vehicles.Price <= 50000) & (vehicles.Mileage <= 300000)]
+    vehicles = vehicles.drop(['Vin'], axis=1)
+
+    vehicles = vehicles.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    for col in ['City', 'State', 'Make', 'Model']:
+        vehicles[col] = vehicles[col].apply(lambda x: x.lower())
+
+    # lets drop null rows
+    vehicles = vehicles.dropna()
+
+    return vehicles
+
+
+def normalize_dataset_frame(vehicles, selector='regression', balanced=True):
+    """
+
+    :type vehicles: object pandas dataframe
+    :param selector: str 'regression' or 'classification'
+    :param balanced: bool balanced class true or false
+
+    :returns trainX, testX, trainY, test_Y
+    """
+
+    if selector == 'classification':
+        if balanced:
+            vehicles['Price'], price_bins = pd.qcut(vehicles.Price, q=data_info['num_class'], labels=False,
+                                                    retbins=True)
+            update_info('price_bins', price_bins)
+        else:
+            vehicles['Price'] = pd.cut(vehicles.Price, bins=data_info['num_class'], labels=False, right=False)
+
+    else:
+        car_price_mean = vehicles['Price'].mean()
+        update_info('car_price_mean', car_price_mean)
+        vehicles['Price'] = (vehicles['Price'] / car_price_mean).astype('float64')
+
+    car_year_min = vehicles['Year'].min()
+    update_info('car_year_min', car_year_min)
+    vehicles['Year'] = (vehicles['Year'] - car_year_min).astype(int)
+
+    car_mileage_mean = vehicles['Mileage'].mean()
+    update_info('car_mileage_mean', car_mileage_mean)
+    vehicles['Mileage'] = (vehicles['Mileage'] / car_mileage_mean).astype('float64')
+
+    label_encoder = {}
+    for col in ['City', 'State', 'Make', 'Model']:
+        label_encoder[col] = LabelEncoder()
+        label_encoder[col].fit(list(vehicles[col].astype(str).values))
+        vehicles[col] = label_encoder[col].transform(list(vehicles[col].astype(str).values))
+        label_encoder[col].get_params()
+    handle_pickle(label_encoder, data_info['label_encoder'])
+
+    target_name = 'Price'
+    train_target = vehicles[target_name]
+
+    vehicles = vehicles.drop([target_name], axis=1)
+    vehicles.sample(5)
+
+    train0, test0, train_target0, test_target0 = train_test_split(vehicles, train_target, test_size=0.2,
+                                                                  random_state=0)
+
+    scaler = StandardScaler()
+    train0 = pd.DataFrame(scaler.fit_transform(train0), columns=train0.columns)
+    test0 = pd.DataFrame(scaler.transform(test0), columns=test0.columns)
+    handle_pickle(scaler, data_info['scaler'])
+
+    # https://stackoverflow.com/a/54508052/2049763
+    train0, train_target0 = train0.to_numpy(), train_target0.to_numpy()
+    test0, test_target0 = test0.to_numpy(), test_target0.to_numpy()
+
+    save_dataset_file(train0, test0, train_target0, test_target0, selector, balanced)
+    return train0, test0, train_target0, test_target0
